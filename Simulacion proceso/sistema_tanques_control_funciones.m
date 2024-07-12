554250729dd5996712d763a95a12a262f779c1ab
carmar14 <carmar141414@hotmail.com>
2024-07-12 11:36:49 -0500

    Simulacion normal
    Simulacion normal. Imagen para paper

--- Simulacion proceso/sistema_tanques_control_funciones.m
+++ Simulacion proceso/sistema_tanques_control_funciones.m
@@ -7,6 +7,8 @@ t=0:3000;
 %referencia acorde al punto de equilibrio Y0=[Y10; Y20;Y30]  Y10=0.400; Y20=0.200; Y30=0.300;
 r1=[0.4*ones(1,250) 0.45*ones(1,1250) 0.4*ones(1,1000) 0.45*ones(1,501)];
 r2=[0.2*ones(1,400) 0.225*ones(1,1600) 0.2*ones(1,1001)];
+r1=[0.35*ones(1,250) 0.42*ones(1,1250) 0.38*ones(1,1000) 0.47*ones(1,501)];
+r2=[0.21*ones(1,400) 0.22*ones(1,1600) 0.18*ones(1,1001)];
 t=t';
 r1=r1'; %vector columna ref 1
 r2=r2'; %vector columna ref 2
@@ -57,18 +59,44 @@ for i=1:n-1
 end
 
 plot(t,x1,'b',t,r1,'--r')
-xlabel('Tiempo (s)')
+grid on
+xlabel('Time(s)')
 ylabel('l_{1}(m)')
 legend('Level 1','q_{1}(m)')
-title('Altura del tanque 1')
+title('Tank Level 1')
 figure
 plot(t,x2,'b',t,r2,'--r')
-xlabel('Tiempo (s)')
+grid on
+xlabel('Time(s)')
 ylabel('l_{2}(m)')
 legend('Level 2','q_{2}(m)')
-title('Altura del tanque 2')
+title('Tank Level 2')
 figure
 plot(t,x3,'b')
-xlabel('Tiempo (s)')
+grid on
+xlabel('Time(s)')
 ylabel('l_{3}(m)')
-title('Altura del tanque 3')
+title('Tank Level 3')
+
+%-----------
+figure
+subplot(3,1,1)
+plot(t,x1,'b',t,r1,'--r')
+grid on
+xlabel('Time(s)')
+ylabel('l_{1}(m)')
+legend('Level 1','q_{1}(m)')
+title('Tank Level 1')
+subplot(3,1,2)
+plot(t,x2,'b',t,r2,'--r')
+grid on
+xlabel('Time(s)')
+ylabel('l_{2}(m)')
+legend('Level 2','q_{2}(m)')
+title('Tank Level 2')
+subplot(3,1,3)
+plot(t,x3,'b')
+grid on
+xlabel('Time(s)')
+ylabel('l_{3}(m)')
+title('Tank Level 3')
